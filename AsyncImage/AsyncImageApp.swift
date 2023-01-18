d8db11b1d347b6b0ba1e179e88366b9182ce5fca
rafroes1 <rafafroes@outlook.com>
2023-01-18 11:29:36 -0300

    Initial Commit

---
+++ AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by Rafael Carvalho on 18/01/23.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
